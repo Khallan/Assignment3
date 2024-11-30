be7ee418841a2d7793b25b1899c1e69f202e9824
Khallan Jamal <khallanjamal1@gmail,com>
2024-11-29 21:49:54 -0500

    T3 Step One -> Book Validations

--- src/LibraryManagmentTest.java
+++ src/LibraryManagmentTest.java
@@ -4,23 +4,19 @@ import org.junit.jupiter.api.Test;
 
 class LibraryManagmentTest {
 
+	//Test case for book ID validity 
 	@Test
 	public void testBookId() throws Exception{
-		Book book1 = new Book (100, null);
-		Book book2 = new Book (999, null);
-		//Book book3 = new Book (10, null);
-		//Book book4 = new Book (1000, null);
+		Book book1 = new Book (100, "Is a valid ID");
+		Book book2 = new Book (999, "Is a valid ID");
 
-		
 		//Checks if the valid numbers are in fact valid
 		assertTrue(book1.isValidId(100));
 		assertTrue(book2.isValidId(999));
-		//Throws exceptions when a number does not fall between 100 to 999
-		assertThrows(Exception.class, () -> new Book(99, "Number is invalid to low. 100 - 999"));
-		assertThrows(Exception.class, () -> new Book(1000, null));
 		
-
-
+		//Throws exceptions when a number does not fall between 100 to 999
+		assertThrows(Exception.class, () -> new Book(99, "Number is invalid to low. Choose between 100 - 999"));
+		assertThrows(Exception.class, () -> new Book(1000, "Number is invalid to high. Choose between 100 - 999"));
 	}
 	
 
